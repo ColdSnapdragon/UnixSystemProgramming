b454f990d433ce3f68db6bd8b51cf0c3f2429731
Blover <1846615034@qq.com>
2022-11-03 01:42:05 +0800

    updata /root 2022-11-03

--- root/gitbk.sh
+++ root/gitbk.sh
@@ -4,12 +4,14 @@ obj="root"
 
 cd /root/Git/UnixSystemProgramming/
 
-git rm -r $obj
-git commit -m "remove old /root $(date +%F)"
-rm -rf $obj
+if [ -d "$obj" ]; then
+	git rm -r $obj
+	git commit -m "remove old /root $(date +%F)"
+	rm -rf $obj
+fi
 
 cp -r /root ./
 git add $obj
-git commit "updata /root $(date +%F)"
+git commit -m "updata /root $(date +%F)"
 
 git push
