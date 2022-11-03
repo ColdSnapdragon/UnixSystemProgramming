b2e90000b05601ea059b85e3fa97df687a6626ae
Blover <1846615034@qq.com>
2022-11-03 13:55:42 +0800

    重置

--- root/hello.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#My first shell program
-
-echo -e 'hello world!\n'
--- root/read.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -t 10 -n 5 -p "input string1(visual,maxlen:5) " str
-echo -e "\noutput: $str\n"  #考虑到read可能截掉换行符，字符串结尾换行
-
-read -t 30 -s -p "input string2(hiden): " str
-echo -e "\noutput: $str"
